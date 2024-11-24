001473a4b6edce1676f70aa9408cf8fe70783a08
soulbgmi153 <pubgabgh@gmail.com>
2024-11-24 11:24:49 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY RAJOWNER90
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7576524422:AAHwgMSordjO_7GlN1-S02Q2-DhL7DvkJzU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAJOWNER90")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1549748318")
